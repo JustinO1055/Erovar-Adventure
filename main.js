0e2b33f0971e55ebda66d7975833337070ef6187
JustinO1055 <thejuiceman1038@gmail.com>
2021-01-19 21:33:19 -0600

    Added skeleton discord bot interface. Ensuring creds are hidden.

---
+++ main.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const login = require('./login.json');
+
+// setting up the client for the bot to connect to discord
+const client = new Discord.Client();
+global.client = client;
+
+// setting a value for the prefix for the bot to look for at the start of a message
+const regExPrefix = /^[a][d][v] /i;
+
+client.once('ready', () => {
+    console.log('Online');
+});
+
+//when a message is sent in the chat, check the message
+client.on('message', message => {
+
+    // if the message matches the set prefix (adv ) and passes the regex, execute the given command
+    if(regExPrefix.test(message.content)){
+
+        // split the message by removing the prefix, split the words by using the delimiter " "
+        const args = message.content.substring(4).split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        if (command == 'ping'){
+            message.reply("pong");
+        }
+
+    }
+
+});
+
+// allows the bot to log in. Token is gathered from login.json
+// token is essentially the password so is kept private
+client.login(login.token);
