be9a8c736fa0aa2b332af0995ba07deb46a3db60
Patrick LeBlanc <leblancpatrick21@gmail.com>
2021-01-22 22:44:20 -0600

    Fixed inventory command. Now working

--- commands/inventory.js
+++ commands/inventory.js
@@ -1,38 +1,45 @@
 const Discord = require('discord.js');
-const {inv, invE} = require('../items.json');
+const {items} = require('../items.json');
 
 module.exports={
     name: 'inventory',
     description: "inventory",
     execute(message){
-        var id;
 
+        //Check if the user mentioned someone else, meaning they want to check another users inventory
         if(message.mentions.members.size > 0){
             id = message.mentions.users.first().id;
         } else {
             id = message.author.id;
         }
 
+        //Get the users inventory
         let sql = `SELECT * FROM Inventory WHERE id = '${id}'`;
-
         connection.query(sql, (err, rows) =>{
             if(err) throw err;
 
+            //Check if the user has an inventory
             if(rows.length < 1){
-                message.channel.send("You better start the journey first you pepeg.")
+                message.channel.send("You better start the journey first. Type 'Adv Start'")
             } else {
                 const User = client.users.cache.get(rows[0].id); // Getting the user by ID.
+
+                //Generate and array of the users inventory.
                 var messy = "";
-                for (let i = 0; i < inv.length; i++) {
-                    messy += invE[i] + " " + inv[i] + ": " + rows[0][inv[i]] + "\n";
+                for(i in items){
+                    //Only show items that have user has at least one of
+                    if(rows[0][items[i]['name']] > 0)
+                        messy += `${items[i]['emoji']} ${items[i]['name']} : ${rows[0][items[i]['name']]} \n`;
                 }
 
+                //Create the embed to output
                 const inventoryEmbed = new Discord.MessageEmbed()
-                    .setColor('#e3750e')
+                    .setColor('#0a008c')
                     .setAuthor(User.username + '\'s Inventory', User.avatarURL())
                     .addFields(
                         { name: 'items', value: messy},
                     )
+                //Send Embed
                 message.channel.send(inventoryEmbed);
             }
         });
