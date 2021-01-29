a0a8c3ba378dd536051c336920fb9ea6cd715b43
JustinO1055 <thejuiceman1038@gmail.com>
2021-01-28 19:01:58 -0600

    added error message/ checking for crafting. Check if user inputs a valid amount of items

--- commands/craft.js
+++ commands/craft.js
@@ -25,6 +25,12 @@ module.exports={
                 amount = calcAmount(args[1]);
         }
 
+        // if the amount is -1 (set for error.) Return and print an error
+        if(amount === -1){
+            message.channel.send(`${message.author}, The amount to craft is invalid. Please give a number to craft. \nValid shorthand notaion \`k=1000\`, \`m=1000000\`, \`b=1000000000\`. Ex. \`adv craft [item] 5k\`. \nType \`adv help\` for help`);
+            return;
+        } 
+
         // set the recipe string to empty string
         let recipe = "";
         
