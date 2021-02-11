bfafb20dc7ffdd633bd97273b6a466da288a7530
Patrick LeBlanc <leblancpatrick21@gmail.com>
2021-02-11 14:00:23 -0600

    Added help messages to some commands.

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 
+// include the json file that holds all of the items names and emoji codes
+const {items} = require('../jsons/items.json');
+
 module.exports={
     name: 'help',
     description: "Give user help",
@@ -9,11 +12,10 @@ module.exports={
             
             //Create the embed to output
             var helpEmbed = new Discord.MessageEmbed()
-                .setColor('#FF69B4')
-                .setDescription("Help will go here");
+                .addFields(
+                    { name: `Help`, value: `Specialized help for help will go here`},
+                );
 
-            //Send Embed
-            message.channel.send(helpEmbed);
             return;
         }
 
@@ -21,44 +23,54 @@ module.exports={
             case 'find':
                 //Create the embed to output
                 var helpEmbed = new Discord.MessageEmbed()
-                    .setColor('#FF69B4')
-                    .setDescription("Specialized help for find will go here");
-    
-                //Send Embed
-                message.channel.send(helpEmbed);
+                    .addFields(
+                        { name: `Find`, value: `Used in area 0 to find tha materials to get your first set of equipment.`},
+                        { name: `Possible Items:`, value: `${items['stick']['emoji']} ${items['log']['emoji']} ${items['pebble']['emoji']} ${items['stone']['emoji']}`},
+                        { name: `Usage:`, value: `\`adv find\``}
+                    );
+
                 break;
             case 'area':
                 //Create the embed to output
                 var helpEmbed = new Discord.MessageEmbed()
-                    .setColor('#FF69B4')
-                    .setDescription("Specialized help for area will go here");
+                    .addFields(
+                        { name: `Area`, value: `Used to move to another area. Usually used if enemies are too strong in your current area or you need to get resources from a specific area.\nNew areas are unlocked by beating the boss in an area.`},
+                        { name: `Usage:`, value: `\`adv area <# of area>\``}
+                    );
     
-                //Send Embed
-                message.channel.send(helpEmbed);
                 break;
             case 'craft':
                 //Create the embed to output
                 var helpEmbed = new Discord.MessageEmbed()
-                    .setColor('#FF69B4')
-                    .setDescription("Specialized help for craft will go here");
-    
-                //Send Embed
-                message.channel.send(helpEmbed);
+                    .addFields(
+                        { name: `Craft`, value: `Used to craft items and equipment. Use the \`recipes\` command to see the list of recipes that can be crafted.\nNote that you cannot carry more than one item of the same equipment type. For example you can only have one sword and one armor at once, so you have to sell your current one to craft another.`},
+                        { name: `Usage:`, value: `\`adv craft <item name> [# to craft]\``}
+                    );
+
                 break;
             case 'disassemble':
             case 'diss':
             case 'da':
                 //Create the embed to output
                 var helpEmbed = new Discord.MessageEmbed()
-                    .setColor('#FF69B4')
-                    .setDescription("Specialized help for disassemble will go here");
+                    .addFields(
+                        { name: `Disasemble`, value: `Used to disasemble an item to get its components back. This will only give back 50% of the recipe cost.`},
+                        { name: `Usage:`, value: `\`adv disassemble <item name> [# to disassemble]\``},
+                        { name: `Alias`, value:  `\`disasemble\` \`diss\` \`da\``}
+                    );
     
-                //Send Embed
-                message.channel.send(helpEmbed);
                 break;
                 
 
         }
+        //Add last details to help embed
+        helpEmbed
+            .setColor('#FF69B4')
+            .setFooter(`Use "adv help" for general help`)
+            
+
+        //Send Embed
+        message.channel.send(helpEmbed);
         
     }
 }
