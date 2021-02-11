dc2efc05c83937a41365a51ef471ad0ada6f4f63
JustinO1055 <thejuiceman1038@gmail.com>
2021-02-10 20:14:02 -0600

    added the battle into boss, computing which move is correct and updating hp accordingly. Need to add win/ loss screens and update DB

--- commands/boss.js
+++ commands/boss.js
@@ -106,7 +106,7 @@ module.exports={
                             return;
                         })
                         .catch(collected => {
-                            message.channel.send('Timeout');
+                            message.channel.send(`Boss fight canceled. ${message.author} has taken too long to respond.`);
                         });
                     });
 
@@ -127,26 +127,212 @@ async function boss0(player, message){
         boss = MONSTERS['area0']['boss'][b];        
     }
 
-    //set the stats for the user for the embed
-    let userStats = `**HP**: ${player.hp}/${player.max_hp}\n**Att**: ${player.attack}\n**Def**: ${player.defence}`;
-
-        // set the stats for the monster for the embed
-    let bossStats = `**HP**: ${boss.hp}/${boss.hp}\n`;
+    // update the user stats
+    userStats = { name: `${message.author.username}'s Stats:`, value: `**HP**: ${player.hp}/${player.max_hp}\n**Att**: ${player.attack}\n**Def**: ${player.defence}`, inline: true };
+    // set the stats for the boss for the embed
+    bossStats = { name: `Boss ${boss.name} ${boss.emoji} Stats:`, value: `**HP**: ${boss.hp}/${boss.hp}\n`, inline: true};
 
     //Decalare player moves for readding to embed
-    let bossPlayerMoves = { name: "Your Moves:", value: "What are you going to do?\n\`attack\` to recklessly attack the monster.\n\`block\` to block the monsters attack and then attack back.\n\`dodge\` to dodge the monsters attack and then attack back.\n\`cry\` to do a big cry."};
-    
+    var playerMoves = { name: "Your Moves:", value: "What are you going to do?\n\`Trap\` to throw a bear trap at the bear.\n\`Tomahawk\` to throw your axe at the bear.\n\`Climb\` To climb a nearby tree.\n\`Block\` to use your shield to block the bear."};
+
+    // randomly generate the move for the boss
+    var bossMove = boss.moves[functions.randomInteger(0,3)];
+    var bossMoveMsg = { name: 'Hint', value : `The ${boss.name} appears to be preparing to ${bossMove}`};
+
+    var outcome;
 
     // create the embed to send
     const bossEmbed = new Discord.MessageEmbed()
     .setColor('#0A008C')
     .setTitle('Boss Fight')
     .addFields(
-        { name: `${message.author.username}'s Stats:`, value: userStats, inline: true },
-        { name: `Boss ${boss.name} ${boss.emoji} Stats:`, value: bossStats, inline: true},
-        bossPlayerMoves
+        userStats,
+        bossStats,
+        playerMoves,
+        bossMoveMsg        
     );
 
-    message.channel.send(bossEmbed);
+    //Declare variables to track current HP for player and monster
+    var playerCurrentHP = player.hp;
+    var bossCurrentHP = boss.hp;
+
+    // set up listening for response
+    let filter = m => m.author.id === message.author.id && (m.content.toLowerCase() == 'trap' || m.content.toLowerCase() == 'tomahawk' || m.content.toLowerCase() == 'climb' || m.content.toLowerCase() == 'block');
+    while(playerCurrentHP > 0 && bossCurrentHP > 0) {
+        message.channel.send(bossEmbed)
+        await message.channel.awaitMessages(filter, {max: 1, time: 30000, errors: ['time'] }).then(mes => {
+            
+            // get the attack choice from user
+            attack = mes.first().content.toLowerCase();
+
+            let outcomeMsg;
+
+            // figure out the outcome for the moves selected
+            // 1 move is good, 1 is okay, 2 are bad
+            /*
+            *
+            *   Boss uses Scratch:      Good: Block
+            *                           Okay: Trap 
+            *                           Bad: Climb, Tomahawk
+            * 
+            *   Boss uses Bite:         Good: Tomahawk. 
+            *                           Okay: Climb
+            *                           Bad: Trap, Block
+            * 
+            *   Boss uses Stomp:        Good: Trap.
+            *                           Okay: Block
+            *                           Bad: Climb, Tomahawk
+            * 
+            *   Boss uses Hibernate:    Good: Climb: 
+            *                           Okay: Tomahawk. 
+            *                           Bad: Trap, Block
+            * 
+            */
+
+            //if else block to find the bosses move
+            if(bossMove == 'Scratch'){
+                // if else block to find the players move
+                // compute the outcome based off the chosen moves
+                if(attack == 'trap'){
+                    // okay outcome, both lose 5 hp
+                    bossCurrentHP -= 5;
+                    playerCurrentHP -= 5;
+                    outcomeMsg = `As the ${boss.name} went to scratch you, you threw a trap partially trapping the bears hand and doing a bit of damage to the ${boss.name} as it still swings and hits you.\n-5 :heart: ${message.author.username}, -5 :heart: ${boss.emoji}`;
+                    
+                } else if(attack == 'tomahawk'){        
+                    // bad outcome, player loses 10 hp
+                    playerCurrentHP -= 10;
+                    outcomeMsg = `You threw your ${functions.getEmoji('stone axe')} at the swinging ${boss.name}, he swats it away and scratches you anyway.\n-10 :heart: ${message.author.username}`;
+
+                } else if(attack == 'climb'){
+                    // bad outcome
+                    playerCurrentHP -= 10;
+                    outcomeMsg = `You decided to try and climb the nearby tree to avoid the ${boss.name}s scratch. Little did you know that he is a good climber as he follows you up the tree and scratches you.\n-10 :heart: ${message.author.username}`;
+
+                } else if(attack == 'block'){
+                    // good outcome
+                    bossCurrentHP -= 10;
+                    outcomeMsg = `You use your ${functions.getEmoji('basic shield')} to block the swiping ${boss.name}. The shield manages to break one of the bears claws!\n-10 :heart: ${boss.emoji}`;
+                    
+                }
+
+
+            } else if (bossMove == 'Bite'){
+                // if else block to find the players move
+                // compute the outcome based off the chosen moves
+                if(attack == 'trap'){
+                    // bad outcome, player losers 10 hp
+                    playerCurrentHP -= 10;
+                    outcomeMsg = `You decided to throw the trap at the ${boss.name} as it was trying to bite you, however you missed and the ${boss.name} bites you.\n-10 :heart: ${message.author.username}`;
+                    
+                } else if(attack == 'tomahawk'){
+                    // good outcome
+                    bossCurrentHP -= 10;
+                    outcomeMsg = `You threw your ${functions.getEmoji('stone axe')} and the ready to bite ${boss.name}, he bites the tomahawk, cutting his mouth and and stopping him from biting you.\n-10 :heart: ${boss.emoji}`;
+
+                } else if(attack == 'climb'){
+                    // okay outcome
+                    playerCurrentHP -= 5;
+                    bossCurrentHP -= 5;
+                    outcomeMsg = `You decided to try and climb the nearby tree to avoid the ${boss.name}s bite. The bear accidently bites the tree hurting his teeth but making you fall down. \n-5 :heart: ${message.author.username}, -5 :heart: ${boss.emoji}`;
+
+                } else if(attack == 'block'){
+                    // bad outcome
+                    playerCurrentHP -= 10;
+                    outcomeMsg = `You attempt you use you ${functions.getEmoji('basic shield')} to block the hungry ${boss.name}. The ${boss.name} does not care as he eats the shield and then bites you.\n-10 :heart: ${message.author.username}`;
+                    
+                }
+
+            
+            } else if (bossMove == 'Stomp'){
+                // if else block to find the players move
+                // compute the outcome based off the chosen moves
+                if(attack == 'trap'){
+                    // good outcome, boss loses 10 hp
+                    bossCurrentHP -= 10;
+                    outcomeMsg = `You threw the trap in the running path of the ${boss.name} causing him to step on it instead of you. The bear was badly hurt.\n -10 :heart: ${boss.emoji}`;
+                    
+                } else if(attack == 'tomahawk'){
+                    // bad outcome
+                    playerCurrentHP -= 10;
+                    outcomeMsg = `You threw your ${functions.getemoji('stone axe')} at the charging ${boss.name}, however you missed and the ${boss.name} stomps on you.\n-10 :heart: ${message.author.username}`;
+
+                } else if(attack == 'climb'){
+                    // bad outcome
+                    playerCurrentHP -= 10;
+                    outcomeMsg = `You decided to try and climb a tree to avoid the charging ${boss.name}. The ${boss.name} did not care that you climbed the tree, climbing after you and stomping you out of the tree.\n-10 :heart: ${message.author.username}`;
+
+                } else if(attack == 'block'){
+                    // okay outcome
+                    playerCurrentHP -= 5;
+                    bossCurrentHP -= 5;
+                    outcomeMsg = `You hold your ${functions.getEmoji('basic shield')} up to block the stomping ${boss.name}. You slightly get squished as the bear twists his elbow on you, slightly hurting himself. \n-5 :heart: ${message.author.username}, -5 :heart: ${boss.emoji}`;
+                    
+                }
+
+            } else if(bossMove == 'Hibernate'){
+                // if else block to find the players move
+                // compute the outcome based off the chosen moves
+                if(attack == 'trap'){
+                    // bad outcome
+                    playerCurrentHP -= 10;
+                    outcomeMsg = `You threw the trap at the hibernating ${boss.name}, this wakes him up causing him to bossMove you.\n-10 :heart: ${message.author.username}`;
+                    
+                } else if(attack == 'tomahawk'){
+                    // okay outcome
+                    playerCurrentHP -= 5;
+                    bossCurrentHP -= 5;
+                    outcomeMsg = `You got a good distance away from the ${boss.name} and threw the ${functions.getEmoji('stone axe')} hitting him in the head causing him to chase after you to attack.\n-5 :heart: ${message.author.username}, -5 :heart: ${boss.emoji}`;
+                } else if(attack == 'climb'){
+                    // good outcome
+                    bossCurrentHP -= 10;
+                    outcomeMsg = `You decided to climb the tree for an ariel attack on the hibernating ${boss.name}. Upon hitting the ${boss.name}, he was left confused and unaware of what had happened. \n-10 :heart: ${boss.emoji}`;
+
+                } else if(attack == 'block'){
+                    //bad outcome
+                    playerCurrentHP -= 10;
+                    outcomeMsg = `You put your ${functions.getEmoji('basic shield')} up to block... the hibernating ${boss.name}. After a while, you get sleepy and doze off as the ${boss.name} wakes up and attacks you. \n-10 :heart: ${message.author.username}`;
+
+                }
+
+            }
+            
+
+            outcome = { name: `Outcome`, value: outcomeMsg, inline: true };
+
+            // update the user stats
+            userStats = { name: `${message.author.username}'s Stats:`, value: `**HP**: ${playerCurrentHP}/${player.max_hp}\n**Att**: ${player.attack}\n**Def**: ${player.defence}`, inline: true };
+            // set the stats for the boss for the embed
+            bossStats = { name: `Boss ${boss.name} ${boss.emoji} Stats:`, value: `**HP**: ${bossCurrentHP}/${boss.hp}\n`, inline: true};
+        
+            // randomly generate the move for the boss
+            bossMove = boss.moves[functions.randomInteger(0,3)];
+            bossMoveMsg = { name: 'Hint', value : `The ${boss.name} appears to be preparing to ${bossMove}`};
+
+            bossFight = [userStats, bossStats, playerMoves, outcome, bossMoveMsg]
+            // update the embed
+            bossEmbed.spliceFields(0, 5, bossFight);
+        })
+        //If the user doesnt enter a valid response, monster attacks
+        .catch(collected => {        
+           
+            playerCurrentHP -= 10;
+            
+            // print the outcome of the event. user didnt respond, boss got angry and attacked for 10hp
+            outcome = { name: `Outcome`, value: `The bear got bored of waiting and attacked you anyway dealing 10 damage.`, inline: true };
+            // update the user stats
+            userStats = { name: `${message.author.username}'s Stats:`, value: `**HP**: ${playerCurrentHP}/${player.max_hp}\n**Att**: ${player.attack}\n**Def**: ${player.defence}`, inline: true };
+            // set the stats for the boss for the embed
+            bossStats = { name: `Boss ${boss.name} ${boss.emoji} Stats:`, value: `**HP**: ${bossCurrentHP}/${boss.hp}\n`, inline: true};
+            // randomly generate the move for the boss
+            bossMove = boss.moves[functions.randomInteger(0,3)];
+            bossMoveMsg = { name: 'Hint', value : `The ${boss.name} appears to be preparing to ${bossMove}`};
+
+            bossFight = [userStats, bossStats, playerMoves, outcome, bossMoveMsg]
+            // update the embed
+            bossEmbed.spliceFields(0, 5, bossFight);
+        });
+    }
+    console.log("hehehe");
 
 }
