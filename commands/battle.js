069a52ab4e84c86d56254917b4e55f4ff6bbee77
Patrick LeBlanc <leblancpatrick21@gmail.com>
2021-02-19 20:29:32 -0600

    Added argument to battle command to skip the prompt

--- commands/battle.js
+++ commands/battle.js
@@ -19,6 +19,11 @@ module.exports={
     description: "Used in order for the player to go on a short mission to fight an enemy. 1 minute cooldown on this command.",
     execute(message, args) {
 
+        if(args[0] === "skip")
+            var skipCheck = true;
+        else
+            var skipCheck = false;
+
         // variables for time cooldown
         var seconds = 0;
         var minutes = 1;
@@ -110,75 +115,34 @@ module.exports={
                         { name: "Action:", value: "What are you going to do?\n\`fight\` to fight the monster.\n\`run\` to run away from the monster."}
                     );
                     
-                    // set up listening for response
-                    let filter = m => m.author.id === message.author.id && (m.content.toLowerCase() == 'fight' || m.content.toLowerCase() == 'run');
-                    message.channel.send(encounterEmbed).then(() => {
-                        message.channel.awaitMessages(filter, {max: 1, time: 30000, errors: ['time'] })
-                            .then(mes => {
-                                //Convert message to lowercase
-                                var command = mes.first().content.toLowerCase();
-
-                                if(command == "fight"){
-                                    //Calculate Monsters new hp
-                                    monsterCurrentHp = functions.calculateDamage(rows[0].attack, monster[2], monster[3], 1);
-
-                                    //Update the Monsters stat on the embed
-                                    encounterStats = `**HP**: ${monsterCurrentHp}/${monster[3]}\n**Att**: ${monster[1]}\n**Def**: ${monster[2]}`;
-                                    encounterMonster = { name: `Wild ${monster[0]}'s ${monster[4]} Stats:`, value: encounterStats, inline: true}
-
-                                    //Calculate players new hp
-                                    playerCurrentHp = functions.calculateDamage(monster[1], rows[0].defence, rows[0].hp, 1);
-
-                                    //Update the Players stat on the embed
-                                    userStats = `**HP**: ${playerCurrentHp}/${rows[0].max_hp}\n**Att**: ${rows[0].attack}\n**Def**: ${rows[0].defence}`;
-                                    encounterPlayer = { name: `${message.author.username}'s Stats:`, value: userStats, inline: true }
-
-                                    //Determine result
-                                    if(playerCurrentHp == 0){
-                                        result = `The wild ${monster[0]} ${monster[4]} has beat you.`
-                                    } else if(monsterCurrentHp == 0){
-                                        result = `You beat the wild ${monster[0]} ${monster[4]}!\nYou got ${monster[5]} XP and ${monster[6]} Gold! from the battle!`;
-                                        // if the drop is not null, drop was obtained, append it to the message
-                                        if(monster[8] != null){
-                                            result += `\nYou have gathered a ${monster[8]} ${items[monster[8]]['emoji']} after defeating the ${monster[0]}.`;
-                                        }
-                                    } else{
-                                        result = `You were unable to beat the wild ${monster[0]} ${monster[4]} and it got away.`
-                                        sqlEncounterResult = `UPDATE Users SET hp = ${playerCurrentHp} WHERE id = '${message.author.id}'`;
-                                        connection.query(sqlEncounterResult);
+                    if(!skipCheck){
+                        //set up listening for response
+                        let filter = m => m.author.id === message.author.id && (m.content.toLowerCase() == 'fight' || m.content.toLowerCase() == 'run');
+                        message.channel.send(encounterEmbed).then(() => {
+                            message.channel.awaitMessages(filter, {max: 1, time: 30000, errors: ['time'] })
+                                .then(mes => {
+                                    //Convert message to lowercase
+                                    var command = mes.first().content.toLowerCase();
+    
+                                    if(command == "fight"){
+                                        battleFight(monster, rows, encounterEmbed, message);
+                                    } else {
+                                        functions.failEscape(monster[4], message.author.id, message.channel.id);
                                     }
-
-                                    //Create result to add to embed
-                                    encounterResult = { name: "Result:", value: result}
-
-                                    //Store all updated values to add to embed
-                                    encounter = [encounterPlayer, encounterMonster, encounterResult];
-
-                                    //Update Embed
-                                    encounterEmbed.spliceFields(0, 3, encounter);
-
-                                    //Send new embed
-                                    message.channel.send(encounterEmbed);
-
-                                    //If the player died, call playerDeath function
-                                    if(playerCurrentHp == 0)
-                                        functions.playerDeath(message, rows[0].level, rows[0].area);
-                                    else if(monsterCurrentHp == 0)
-                                        functions.battleSuccess(message, rows[0].level, rows[0].xp, monster[5], playerCurrentHp, monster[6], monster[8]);
-                                    
-                                } else {
+    
+    
+                                })
+                                //If the user doesnt enter a valid response, monster attacks
+                                .catch(collected => {
+                                    message.channel.send(`After staring at the moster for a while, you decide to try and run away.`);
                                     functions.failEscape(monster[4], message.author.id, message.channel.id);
-                                }
-
-
-                            })
-                            //If the user doesnt enter a valid response, monster attacks
-                            .catch(collected => {
-                                message.channel.send(`After staring at the moster for a while, you decide to try and run away.`);
-                                functions.failEscape(monster[4], message.author.id, message.channel.id);
+                                });
                             });
-                        });
-
+    
+                    
+                    } else{
+                        battleFight(monster, rows, encounterEmbed, message);
+                    }
                 });
         
                 // update the cooldown in database
@@ -188,4 +152,54 @@ module.exports={
         });
 
     }
+}
+
+//Function for determining outcome of fighting in the battle
+function battleFight(monster, rows, encounterEmbed, message){
+    //Calculate Monsters new hp
+    monsterCurrentHp = functions.calculateDamage(rows[0].attack, monster[2], monster[3], 1);
+    
+    //Update the Monsters stat on the embed
+    encounterStats = `**HP**: ${monsterCurrentHp}/${monster[3]}\n**Att**: ${monster[1]}\n**Def**: ${monster[2]}`;
+    encounterMonster = { name: `Wild ${monster[0]}'s ${monster[4]} Stats:`, value: encounterStats, inline: true}
+
+    //Calculate players new hp
+    playerCurrentHp = functions.calculateDamage(monster[1], rows[0].defence, rows[0].hp, 1);
+
+    //Update the Players stat on the embed
+    userStats = `**HP**: ${playerCurrentHp}/${rows[0].max_hp}\n**Att**: ${rows[0].attack}\n**Def**: ${rows[0].defence}`;
+    encounterPlayer = { name: `${message.author.username}'s Stats:`, value: userStats, inline: true }
+
+    //Determine result
+    if(playerCurrentHp == 0){
+        result = `The wild ${monster[0]} ${monster[4]} has beat you.`
+    } else if(monsterCurrentHp == 0){
+        result = `You beat the wild ${monster[0]} ${monster[4]}!\nYou got ${monster[5]} XP and ${monster[6]} Gold! from the battle!`;
+        // if the drop is not null, drop was obtained, append it to the message
+        if(monster[8] != null){
+            result += `\nYou have gathered a ${monster[8]} ${items[monster[8]]['emoji']} after defeating the ${monster[0]}.`;
+        }
+    } else{
+        result = `You were unable to beat the wild ${monster[0]} ${monster[4]} and it got away.`
+        sqlEncounterResult = `UPDATE Users SET hp = ${playerCurrentHp} WHERE id = '${message.author.id}'`;
+        connection.query(sqlEncounterResult);
+    }
+
+    //Create result to add to embed
+    encounterResult = { name: "Result:", value: result}
+
+    //Store all updated values to add to embed
+    encounter = [encounterPlayer, encounterMonster, encounterResult];
+
+    //Update Embed
+    encounterEmbed.spliceFields(0, 3, encounter);
+
+    //Send new embed
+    message.channel.send(encounterEmbed);
+
+    //If the player died, call playerDeath function
+    if(playerCurrentHp == 0)
+        functions.playerDeath(message, rows[0].level, rows[0].area);
+    else if(monsterCurrentHp == 0)
+        functions.battleSuccess(message, rows[0].level, rows[0].xp, monster[5], playerCurrentHp, monster[6], monster[8]);
 }
